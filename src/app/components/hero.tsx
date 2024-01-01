11ce1bed107c350a6e1a4f13bc1daa7c0b5150cf
anthony <devgayflor@gmail.com>
2023-12-31 23:06:49 -0500

    added container to hero section

--- src/app/components/hero.tsx
+++ src/app/components/hero.tsx
@@ -8,7 +8,7 @@ export default function HeroSection() {
         <img src={'/Assets/alloys-origin-hero.png'} alt={'/Alloys Origin Hero'} width={0} height={0} className='-z-10 w-full h-full object-cover object-left' />
       </div>
 
-      <div className='text-container z-10 translate-y-[60px]'>
+      <div className='text-container z-10 translate-y-[60px] container'>
         <h1 className='title-2xl-upper'>HYPER SETS</h1>
         <h4 className='sub-text'>Community collection of HyperX NGenuity RGB presets</h4>
         <div className='translate-y-[25px]'>
